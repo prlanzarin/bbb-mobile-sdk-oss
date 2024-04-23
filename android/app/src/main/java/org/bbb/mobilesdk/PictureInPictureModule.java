b8cc55a976aff7e765d2e5feb9b1c0956541ef4f
Max <max.franke@acad.pucrs.br>
2024-04-23 03:12:25 -0300

    feat(PiP): enable autoEnterEnabled

--- android/app/src/main/java/org/bbb/mobilesdk/PictureInPictureModule.java
+++ android/app/src/main/java/org/bbb/mobilesdk/PictureInPictureModule.java
@@ -101,6 +101,7 @@ class PictureInPictureModule extends ReactContextBaseJavaModule {
 
         PictureInPictureParams.Builder builder
                 = new PictureInPictureParams.Builder()
+                .setAutoEnterEnabled(true)
                 .setAspectRatio(new Rational(1, 1));
 
         // https://developer.android.com/reference/android/app/Activity.html#enterPictureInPictureMode(android.app.PictureInPictureParams)
