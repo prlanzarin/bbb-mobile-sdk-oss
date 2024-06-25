b944bcb45bf3496033641164d9c2951d26653008
Max <max.franke@acad.pucrs.br>
2024-06-25 09:29:47 -0300

    fix(user-participants): add equal function to selector

--- src/utils/functions/index.js
+++ src/utils/functions/index.js
@@ -15,6 +15,11 @@ const humanizeSeconds = (time) => {
   },).join(':');
 };
 
+const arraysEqual = (a1,a2) => {
+  return JSON.stringify(a1)==JSON.stringify(a2);
+}
+
 export default {
   humanizeSeconds,
+  arraysEqual
 };
