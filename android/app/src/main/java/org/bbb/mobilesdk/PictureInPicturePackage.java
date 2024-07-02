8c81c3029d30f00678a9e9f50e6ff1908e9f648f
Max <max.franke@acad.pucrs.br>
2024-07-02 18:25:41 -0300

    feat(pip): comment PiP for portal build

--- android/app/src/main/java/org/bbb/mobilesdk/PictureInPicturePackage.java
+++ android/app/src/main/java/org/bbb/mobilesdk/PictureInPicturePackage.java
@@ -1,28 +1,27 @@
-package org.bbb.mobilesdk;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
+// package org.bbb.mobilesdk;
+// import com.facebook.react.ReactPackage;
+// import com.facebook.react.bridge.NativeModule;
+// import com.facebook.react.bridge.ReactApplicationContext;
+// import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
 
-public class PictureInPicturePackage implements ReactPackage {
+// public class PictureInPicturePackage /*implements*/ ReactPackage {
 
-   @Override
-   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-       return Collections.emptyList();
-   }
+//    @Override
+//    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+//        return Collections.emptyList();
+//    }
 
-   @Override
-   public List<NativeModule> createNativeModules(
-           ReactApplicationContext reactContext) {
-       List<NativeModule> modules = new ArrayList<>();
+//    @Override
+//    public List<NativeModule> createNativeModules(
+//            ReactApplicationContext reactContext) {
+//        List<NativeModule> modules = new ArrayList<>();
 
-       modules.add(new PictureInPictureModule(reactContext));
+//        modules.add(new PictureInPictureModule(reactContext));
 
-       return modules;
-   }
-
-}
+//        return modules;
+//    }
+// }
