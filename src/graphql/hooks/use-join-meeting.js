e665e90f402b0916ae7e53a974bf0e6b06a7fe58
Max <max.franke@acad.pucrs.br>
2024-07-12 16:40:28 -0300

    feat(use-join-meeting): add hook
    Co-authored-by: matheus-lagreca <matheus.lagreca@gmail.com>

---
+++ src/graphql/hooks/use-join-meeting.js
@@ -0,0 +1,259 @@
+import { useEffect, useState, useRef } from 'react';
+import {
+  ApolloClient, InMemoryCache, ApolloLink,
+} from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { onError } from '@apollo/client/link/error';
+import UrlUtils from '../../utils/functions';
+
+const useJoinMeeting = () => {
+  const [loginStage, setLoginStage] = useState(0);
+  const [sessionToken, setSessionToken] = useState(null);
+  const [urlWithSessionId, setUrlWithSessionId] = useState(null);
+  const [graphqlUrlApolloClient, setApolloClient] = useState(null);
+  const [graphqlWebsocketUrl, setGraphqlWebsocketUrl] = useState(null);
+  const [graphqlApiUrl, setGraphqlApiUrl] = useState(null);
+  const [clientStartupSettings, setClientStartupSettings] = useState(null);
+  const [clientSettings, setClientSettings] = useState(null);
+  const [host, setHost] = useState('');
+  const activeSocket = useRef();
+  const numberOfAttempts = useRef(20);
+  const tsLastMessageRef = useRef(0);
+  const tsLastPingMessageRef = useRef(0);
+
+  const url = '';
+
+  async function requestSessionToken() {
+    fetch(`${url}`)
+      .then((data) => {
+        if (data.status === 200) {
+          setUrlWithSessionId(data.url);
+          setSessionToken(UrlUtils.parseQueryString(data.url).sessionToken);
+          setHost(UrlUtils.getHostFromUrl(url));
+          console.log('DONE STAGE 0');
+          setLoginStage(1);
+        }
+      })
+      .catch((error) => console.error('error requestSessionToken()', error));
+  }
+
+  async function joinWithSessionToken() {
+    fetch(`${urlWithSessionId}`)
+      .then((data) => {
+        if (data.status === 200) {
+          setLoginStage(2);
+          console.log('DONE STAGE 1');
+        }
+      })
+      .catch((error) => console.error('error joinWithSessionToken()', error));
+  }
+
+  async function callApi() {
+    fetch(`https://${host}/bigbluebutton/api`, {
+      credentials: 'include',
+    })
+      .then((response) => response.text())
+      .then((xmlString) => {
+        try {
+          const data = UrlUtils.xml2json(xmlString);
+          const gqWs = data.graphqlWebsocketUrl;
+          const gqUrl = data.graphqlApiUrl;
+          setGraphqlApiUrl(gqUrl);
+          setGraphqlWebsocketUrl(gqWs);
+          console.log('DONE STAGE 2');
+          setLoginStage(3);
+        } catch (error) {
+          console.error('Error parsing callApi() response', error);
+        }
+      })
+      .catch((error) => {
+        console.error('error callApi()', error);
+      });
+  }
+
+  async function getClientStartupSettings() {
+    fetch(`${graphqlApiUrl}/clientStartupSettings`, {
+      credentials: 'include',
+      headers: {
+        'X-Session-Token': sessionToken,
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setClientStartupSettings(data.meeting_clientSettings[0]);
+        console.log('DONE STAGE 3');
+        setLoginStage(4);
+      })
+      .catch((error) => console.error('error getClientStartupSettings()', error));
+  }
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message }) => {
+        console.error(`[GraphQL error]: Message: ${message}`);
+      });
+    }
+
+    if (networkError) {
+      console.error(`[Network error]: ${networkError}`);
+    }
+  });
+
+  const estimatePayloadSize = (variables) => {
+    const variablesAsString = JSON.stringify(variables);
+    // eslint-disable-next-line no-undef
+    const variablesAsBlob = new Blob([variablesAsString]);
+    return variablesAsBlob.size;
+  };
+
+  const payloadSizeCheckLink = new ApolloLink((operation, forward) => {
+    if (operation.query.definitions.some((def) => 'operation' in def && def.operation === 'mutation')) {
+      const size = estimatePayloadSize(operation.variables);
+      const maxPayloadSize = 10485760; // 10MB
+
+      if (size > maxPayloadSize) {
+        const errorMsg = `Mutation payload is too large: ${size} bytes. ${maxPayloadSize} maximum allowed.`;
+        console.error(errorMsg);
+        return null;
+      }
+    }
+
+    console.log(`Valid ${operation.operationName} payload. Following with the query.`);
+    return forward(operation);
+  });
+
+  async function connectGraphqlServer() {
+    if (sessionToken == null) return;
+    const clientSessionUUID = '1234';
+
+    let wsLink;
+    try {
+      const subscription = createClient({
+        url: graphqlWebsocketUrl,
+        retryAttempts: numberOfAttempts.current,
+        keepAlive: 99999999999,
+        retryWait: async () => {
+          return new Promise((res) => {
+            setTimeout(() => {
+              res();
+            }, 10000);
+          });
+        },
+        shouldRetry: (error) => {
+          if (error.code === 4403) {
+            console.error('Session token is invalid', error);
+            return false;
+          }
+          return true;
+        },
+        connectionParams: {
+          headers: {
+            'X-Session-Token': sessionToken,
+            'X-ClientSessionUUID': clientSessionUUID,
+            'X-ClientType': 'HTML5',
+            'X-ClientIsMobile': 'true',
+          },
+        },
+        on: {
+          error: (error) => {
+            console.error(`Error: on subscription to server: ${error}`);
+          },
+          closed: () => {
+            console.error('socket closed');
+          },
+          connected: (socket) => {
+            activeSocket.current = socket;
+
+            console.log('socket connected');
+          },
+          connecting: () => {
+            console.log('socket connecting');
+          },
+          message: (message) => {
+            if (message.type === 'ping') {
+              tsLastPingMessageRef.current = Date.now();
+            }
+            tsLastMessageRef.current = Date.now();
+          },
+
+        },
+      });
+      const graphWsLink = new GraphQLWsLink(
+        subscription,
+      );
+      wsLink = ApolloLink.from([payloadSizeCheckLink, errorLink, graphWsLink]);
+      wsLink.setOnError((error) => {
+        throw new Error('Error: on apollo connection'.concat(JSON.stringify(error) || ''));
+      });
+    } catch (error) {
+      throw new Error('Error creating WebSocketLink: '.concat(JSON.stringify(error) || ''));
+    }
+    let client;
+    try {
+      client = new ApolloClient({
+        link: wsLink,
+        cache: new InMemoryCache(),
+        connectToDevTools: true,
+      });
+      setApolloClient(client);
+      console.log('DONE STAGE 4');
+      setLoginStage(5);
+    } catch (error) {
+      throw new Error('Error creating Apollo Client: '.concat(JSON.stringify(error) || ''));
+    }
+  }
+
+  async function getClientSettings() {
+    fetch(`${graphqlApiUrl}/clientSettings`, {
+      credentials: 'include',
+      headers: {
+        'X-Session-Token': sessionToken,
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setClientSettings(data.meeting_clientSettings[0].clientSettingsJson.public);
+        console.log('DONE STAGE 5');
+        setLoginStage(6);
+      });
+  }
+
+  useEffect(() => {
+    switch (loginStage) {
+      case 0:
+        requestSessionToken();
+        break;
+      case 1:
+        joinWithSessionToken();
+        break;
+      case 2:
+        callApi();
+        break;
+      case 3:
+        getClientStartupSettings();
+        break;
+      case 4:
+        connectGraphqlServer();
+        break;
+      case 5:
+        getClientSettings();
+        break;
+      case 6:
+        console.log('LOGIN COMPLETE');
+        break;
+      default:
+        console.log('error');
+    }
+  }, [loginStage]);
+
+  return {
+    graphqlUrlApolloClient,
+    sessionToken,
+    loginStage,
+    clientStartupSettings,
+    clientSettings
+  };
+};
+
+export default useJoinMeeting;
--- src/utils/functions/index.js
+++ src/utils/functions/index.js
@@ -15,11 +15,67 @@ const humanizeSeconds = (time) => {
   },).join(':');
 };
 
-const arraysEqual = (a1,a2) => {
-  return JSON.stringify(a1)==JSON.stringify(a2);
+const arraysEqual = (a1, a2) => {
+  // eslint-disable-next-line eqeqeq
+  return JSON.stringify(a1) == JSON.stringify(a2);
+};
+
+const parseQueryString = (url) => {
+  const queryString = url.split('?')[1];
+  if (!queryString) {
+    return {};
+  }
+
+  const params = {};
+  const keyValuePairs = queryString.split('&');
+
+  keyValuePairs.forEach((pair) => {
+    const [key, value] = pair.split('=');
+    params[key] = decodeURIComponent(value || '');
+  });
+
+  return params;
+};
+
+const getHostFromUrl = (url) => {
+  const regex = /^(?:[^:\n]+:\/\/)?([^:#/\n]*)/;
+  const match = url.match(regex);
+  const host = match ? match[1] : null;
+  return host;
+};
+
+function xml2json(xmlString) {
+  const responseRegex = /<response>([\s\S]*?)<\/response>/;
+  const elementRegex = /<([a-zA-Z0-9_-]+)>([\s\S]*?)<\/\1>/g;
+
+  const responseMatch = responseRegex.exec(xmlString);
+  if (responseMatch) {
+    const responseData = responseMatch[1];
+
+    let match;
+    const result = {};
+
+    // eslint-disable-next-line no-cond-assign
+    while ((match = elementRegex.exec(responseData)) !== null) {
+      const elementName = match[1];
+      const elementValue = match[2].trim();
+
+      if (result[elementName] === undefined) {
+        result[elementName] = [];
+      }
+
+      result[elementName] = elementValue;
+    }
+
+    return result;
+  }
+  console.error('No <response> tag found in XML');
 }
 
 export default {
   humanizeSeconds,
-  arraysEqual
+  arraysEqual,
+  parseQueryString,
+  getHostFromUrl,
+  xml2json
 };
