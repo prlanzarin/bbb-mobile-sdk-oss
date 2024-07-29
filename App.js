a86fc17a94dceac00481daa031f40a435bcdf52b
Max <max.franke@acad.pucrs.br>
2024-07-29 10:45:46 -0300

    fix(app): set default screen background color

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { Provider } from 'react-redux';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { OrientationLocker, PORTRAIT } from 'react-native-orientation-locker';
 import { store } from './src/store/redux/store';
 import AppStatusBar from './src/components/status-bar';
 import './src/utils/locales/i18n';
 import MainNavigator from './src/screens/main-navigator';
+import Colors from './src/constants/colors';
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: Colors.blueBackgroundColor
+  },
+};
 
 const App = (props) => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <NavigationContainer theme={MyTheme}>
         <OrientationLocker orientation={PORTRAIT} />
         <MainNavigator {...props} />
         <AppStatusBar />
