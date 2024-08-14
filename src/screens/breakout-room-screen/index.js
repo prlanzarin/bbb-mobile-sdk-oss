9dcb8c11b49868d17918bdc6fe708b640064a07b
Matheus Lagreca <matheus.lagreca@gmail.com>
2024-08-14 10:21:22 -0300

    feat(breakouts): add skeleton loading

--- src/screens/breakout-room-screen/index.js
+++ src/screens/breakout-room-screen/index.js
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { useCallback, useEffect, useState } from 'react';
+import { Dimensions } from 'react-native';
 import { useOrientation } from '../../hooks/use-orientation';
 import AudioManager from '../../services/webrtc/audio-manager';
 import VideoManager from '../../services/webrtc/video-manager';
@@ -16,14 +17,25 @@ import {
   FIRST_BREAKOUT_DURATION_DATA_SUBSCRIPTION
 } from './queries.js'
 
+const DEVICE_HEIGHT = parseInt(Dimensions.get("window").height, 10);
+const DEVICE_WIDTH = parseInt(Dimensions.get("window").width, 10);
+
 //TODO: move breakoutTimeRemaining to a component
 const BreakoutRoomScreen = () => {
   const localCameraId = useSelector((state) => state.video.localCameraId);
   const [time, setTime] = useState(-100);
   const [requestedUrl, setRequestedUrl] = useState(false);
 
-  const { data: breakoutData, loading: breakoutLoading, error: breakoutError } = useSubscription(BREAKOUT_ROOM_SUBSCRIPTION);
-  const { data: breakoutTimeData, loading: breakoutTimeLoading, error: breakoutTimeError } = useSubscription(FIRST_BREAKOUT_DURATION_DATA_SUBSCRIPTION);
+  const {
+    data: breakoutData,
+    loading: breakoutLoading,
+    error: breakoutError,
+  } = useSubscription(BREAKOUT_ROOM_SUBSCRIPTION);
+  const {
+    data: breakoutTimeData,
+    loading: breakoutTimeLoading,
+    error: breakoutTimeError,
+  } = useSubscription(FIRST_BREAKOUT_DURATION_DATA_SUBSCRIPTION);
   const [dispatchRequestJoinUrl] = useMutation(BREAKOUT_ROOM_REQUEST_JOIN_URL);
 
   const breakoutRoom = breakoutData?.breakoutRoom || [];
@@ -31,8 +43,11 @@ const BreakoutRoomScreen = () => {
   const breakoutDuration = breakoutTimeData?.breakoutRoom[0]?.durationInSeconds;
   const breakoutStartedAt = breakoutTimeData?.breakoutRoom[0]?.startedAt;
   const breakoutStartedTime = new Date(breakoutStartedAt).getTime();
-  const endTime = breakoutStartedTime + (breakoutDuration * 1000);
-  const breakoutTimeRemaining = Math.max(0, Math.floor((endTime - Date.now()) / 1000));
+  const endTime = breakoutStartedTime + breakoutDuration * 1000;
+  const breakoutTimeRemaining = Math.max(
+    0,
+    Math.floor((endTime - Date.now()) / 1000),
+  );
 
   const orientation = useOrientation();
   const navigation = useNavigation();
@@ -41,16 +56,16 @@ const BreakoutRoomScreen = () => {
   useEffect(() => {
     if (!breakoutLoading) {
       if (breakoutError) {
-        console.error("BreakoutError: ", breakoutError)
-      };
+        console.error("BreakoutError: ", breakoutError);
+      }
     }
   }, [breakoutData, breakoutLoading, breakoutError]);
 
   useEffect(() => {
     if (!breakoutTimeLoading) {
       if (breakoutTimeError) {
-        console.error("BreakoutTimeError: ", breakoutTimeError)
-      };
+        console.error("BreakoutTimeError: ", breakoutTimeError);
+      }
     }
   }, [breakoutTimeData, breakoutTimeLoading, breakoutTimeError]);
 
@@ -84,9 +99,9 @@ const BreakoutRoomScreen = () => {
   const handleDispatchRequestJoinUrl = (breakoutRoomId) => {
     dispatchRequestJoinUrl({
       variables: {
-        breakoutRoomId
-      }
-    })
+        breakoutRoomId,
+      },
+    });
   };
 
   // ***** FUNCTIONS *****
@@ -102,7 +117,7 @@ const BreakoutRoomScreen = () => {
   const handleJoinButton = (breakoutId, breakoutRoomJoinUrl) => {
     if (!breakoutRoomJoinUrl) {
       setRequestedUrl(true);
-      handleDispatchRequestJoinUrl(breakoutId)
+      handleDispatchRequestJoinUrl(breakoutId);
       setTimeout(() => setRequestedUrl(false), 3000);
     } else {
       joinSession(breakoutRoomJoinUrl);
@@ -114,12 +129,15 @@ const BreakoutRoomScreen = () => {
   const renderUsersJoinedMiniAvatar = (participants) => {
     return (
       <Styled.MiniAvatarsContainer participantsCount={participants.length}>
-        {
-          participants.slice(0, 3).map((item, idx) => (
-            // the userId in breakout room has a -\S+/ after the original userId
-            <Styled.MiniAvatar userName={item.user.name} mini userId={item.userId.replace(/-\S+/, '')} key={item.userId + idx} />
-          ))
-        }
+        {participants.slice(0, 3).map((item, idx) => (
+          // the userId in breakout room has a -\S+/ after the original userId
+          <Styled.MiniAvatar
+            userName={item.user.name}
+            mini
+            userId={item.userId.replace(/-\S+/, "")}
+            key={item.userId + idx}
+          />
+        ))}
       </Styled.MiniAvatarsContainer>
     );
   };
@@ -128,25 +146,31 @@ const BreakoutRoomScreen = () => {
     return (
       <Styled.Card>
         <ExpandedCard
-          content={(
+          content={
             <>
               <Styled.ShortName>{item.shortName}</Styled.ShortName>
               <Styled.ParticipantsContainer>
                 {renderUsersJoinedMiniAvatar(item.participants)}
                 <Styled.ParticipantsCount>
-                  {`${item.participants.length} ${t('mobileSdk.breakout.participantsLabel')}`}
+                  {`${item.participants.length} ${t("mobileSdk.breakout.participantsLabel")}`}
                 </Styled.ParticipantsCount>
               </Styled.ParticipantsContainer>
             </>
-          )}
-          expandedContent={(
+          }
+          expandedContent={
             <>
               <Styled.DividerTinyBottom />
               {item.participants.map((user, idx) => {
                 return (
-                  <Styled.ParticipantsContainerExpandable key={user.userId + idx}>
+                  <Styled.ParticipantsContainerExpandable
+                    key={user.userId + idx}
+                  >
                     {/* // the userId in breakout room has a -\S+/ after the original userId */}
-                    <Styled.MiniAvatar userName={user.user.name} mini userId={user.userId.replace(/-\S+/, '')} />
+                    <Styled.MiniAvatar
+                      userName={user.user.name}
+                      mini
+                      userId={user.userId.replace(/-\S+/, "")}
+                    />
                     <Styled.UserNameText>{user.user.name}</Styled.UserNameText>
                   </Styled.ParticipantsContainerExpandable>
                 );
@@ -154,14 +178,18 @@ const BreakoutRoomScreen = () => {
 
               <Styled.ButtonContainer>
                 <Styled.JoinBreakoutButton
-                  onPress={() => handleJoinButton(item.breakoutRoomId, item.joinURL)}
+                  onPress={() =>
+                    handleJoinButton(item.breakoutRoomId, item.joinURL)
+                  }
                   loading={requestedUrl}
                 >
-                  {item.joinURL ? t('mobileSdk.breakout.join') : t('mobileSdk.breakout.askToJoin')}
+                  {item.joinURL
+                    ? t("mobileSdk.breakout.join")
+                    : t("mobileSdk.breakout.askToJoin")}
                 </Styled.JoinBreakoutButton>
               </Styled.ButtonContainer>
             </>
-          )}
+          }
           // 30px for each participant and 90 for the button
           expandableHeight={item.participants.length * 30 + 90}
         />
@@ -209,20 +237,27 @@ const BreakoutRoomScreen = () => {
     );
   };
 
-  if (!hasBreakouts) {
+  if (breakoutLoading) {
+    return (
+      <Styled.BreakoutsSkeletonLoading
+        DEVICE_HEIGHT={DEVICE_HEIGHT - 60}
+        DEVICE_WIDTH={DEVICE_WIDTH}
+      />
+    );
+  } else if (!hasBreakouts && !breakoutLoading) {
     return (
       <ScreenWrapper>
         <Styled.ContainerCentralizedView>
           <Styled.NoBreakoutsImage
-            source={require('../../assets/application/service-off.png')}
+            source={require("../../assets/application/service-off.png")}
             resizeMode="contain"
             style={{ width: 173, height: 130 }}
           />
           <Styled.NoBreakoutsLabelTitle>
-            {t('mobileSdk.breakout.noBreakoutsTitle')}
+            {t("mobileSdk.breakout.noBreakoutsTitle")}
           </Styled.NoBreakoutsLabelTitle>
           <Styled.NoBreakoutsLabelSubtitle>
-            {t('mobileSdk.breakout.noBreakoutsSubtitle')}
+            {t("mobileSdk.breakout.noBreakoutsSubtitle")}
           </Styled.NoBreakoutsLabelSubtitle>
         </Styled.ContainerCentralizedView>
       </ScreenWrapper>
