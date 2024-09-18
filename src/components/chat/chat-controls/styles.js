d327a9654d89fefb6e44c6f188d345f9e9538b9e
Max <max.franke@acad.pucrs.br>
2024-09-18 17:42:47 -0300

    feat(chat-controls): change design

--- src/components/chat/chat-controls/styles.js
+++ src/components/chat/chat-controls/styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
-import iconButton from '../../icon-button/index';
+import IconButton from '../../icon-button/index';
+import Colors from '../../../constants/colors';
 
-const NotificationIcon = styled(iconButton)`
+const NotificationIcon = styled(IconButton)`
   position: absolute;
   padding: 0px;
   margin: 0px;
@@ -16,7 +17,30 @@ const Container = styled.View`
   position: relative;
 `;
 
+const ChatButton = ({ onPress, hasUnreadMessages, isBottomChatOpen }) => {
+  return (
+    <Container>
+      <IconButton
+        size={32}
+        icon="message-outline"
+        iconColor={isBottomChatOpen ? Colors.blueIconColor : Colors.lightGray300}
+        containerColor={isBottomChatOpen ? Colors.white : Colors.lightGray200}
+        animated
+        onPress={onPress}
+      />
+      {hasUnreadMessages && (
+        <NotificationIcon
+          icon="circle"
+          size={12}
+          iconColor={Colors.orange}
+        />
+      )}
+    </Container>
+  );
+};
+
 export default {
   NotificationIcon,
   Container,
+  ChatButton,
 };
